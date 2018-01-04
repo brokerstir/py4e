0a8f49f9bf4c866f09cc6a61c326005248133d04
brokerstir <take1risk@gmail.com>
2018-01-03 21:24:42 -0500

    initial commit

---
+++ Most_Prolific_Commiter.py
@@ -0,0 +1,28 @@
+# 9.4 Write a program to read through the mbox-short.txt
+# and figure out who has the sent the greatest number of mail messages.
+# The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail.
+# The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file.
+# After the dictionary is produced,
+# the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    if not line.startswith("From ") : continue
+    words = line.split()
+    word = (words[1])
+    #for word in words:
+    counts[word] = counts.get(word,0) + 1
+#print(counts)
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
