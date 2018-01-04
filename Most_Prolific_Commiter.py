d364237846d8be5329b75127d5f4687a87ac921b
brokerstir <take1risk@gmail.com>
2018-01-04 04:56:01 -0500

    added comments

--- Most_Prolific_Commiter.py
+++ Most_Prolific_Commiter.py
@@ -5,22 +5,32 @@
 # After the dictionary is produced,
 # the program reads through the dictionary using a maximum loop to find the most prolific committer.
 
+# Get file from user and open file
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
 
+# Define dictionary
 counts = dict()
+
+# Loop through each line in file
 for line in handle:
+    # Skip out of loop if line not start with From
     if not line.startswith("From ") : continue
+    # Split line at each space
     words = line.split()
+    # Define second sring in line as word
     word = (words[1])
-    #for word in words:
+    # Increment the nunber of times that word appers
     counts[word] = counts.get(word,0) + 1
 #print(counts)
 
+# Define variables
 bigcount = None
 bigword = None
+# Loop through dictionary
 for word,count in counts.items():
+    # Find the word that with highest count
     if bigcount is None or count > bigcount:
         bigword = word
         bigcount = count
