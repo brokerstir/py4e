c3fb06e48ea166dd9e93f29f45d1536efc3e432f
brokerstir <take1risk@gmail.com>
2018-06-20 04:51:58 -0500

    initial commit

--- datacamp-py/cleaning_data_ch1_exploring.py
+++ datacamp-py/cleaning_data_ch1_exploring.py
@@ -1,3 +1,11 @@
+'''
+Loading and viewing your data
+In this chapter, you're going to look at a subset of the Department of Buildings Job Application Filings dataset from the NYC Open Data portal. This dataset consists of job applications filed on January 22, 2017.
+Your first task is to load this dataset into a DataFrame and then inspect it using the .head() and .tail() methods. However, you'll find out very quickly that the printed results don't allow you to see everything you need, since there are too many columns. Therefore, you need to look at the data in another way. 
+The .shape and .columns attributes let you see the shape of the DataFrame and obtain a list of its columns. From here, you can see which columns are relevant to the questions you'd like to ask of the data. To this end, a new DataFrame, df_subset, consisting only of these relevant columns, has been pre-loaded. This is the DataFrame you'll work with in the rest of the chapter.
+Get acquainted with the dataset now by exploring it with pandas! This initial exploratory analysis is a crucial first step of data cleaning.
+'''
+
 # Import pandas
 import pandas as pd
 
@@ -20,6 +28,17 @@ print(df.columns)
 print(df_subset.head())
 print(df_subset.tail())
 
+'''
+The .info() method provides important information about a DataFrame, such as the number of rows, number of columns, number of non-missing values in each column, and the data type stored in each column. This is the kind of information that will allow you to confirm whether the 'Initial Cost' and 'Total Est. Fee' columns are numeric or strings. From the results, you'll also be able to see whether or not all columns have complete data in them. 
+'''
+
+'''
+You'll now use the .describe() method to calculate summary statistics of your data.
+'''
+
+'''Frequency counts for categorical data
+As you've seen, .describe() can only be used on numeric columns. So how can you diagnose data issues when you have categorical data? One way is by using the .value_counts() method, which returns the frequency counts for each unique value in a column!
+This method also has an optional parameter called dropna which is True by default. What this means is if you have missing data in a column, it will not give a frequency count of them. You want to set the dropna column to False so if there are missing values in a column, it will give you the frequency counts.'''
 
 
 # Print the info of df
