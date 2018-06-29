ae72189b6c084aad01822a3f644c7c5bc7075ab2
brokerstir <take1risk@gmail.com>
2018-06-29 08:34:01 -0500

    adding notes

--- datacamp-py/cleaning_data_ch2_tidy_data.py
+++ datacamp-py/cleaning_data_ch2_tidy_data.py
@@ -1,3 +1,26 @@
+'''Recognizing tidy data
+For data to be tidy, it must have:
+Each variable as a separate column.
+Each row as a separate observation.
+As a data scientist, you'll encounter data that is represented in a variety of different ways, so it is important to be able to recognize tidy (or untidy) data when you see it.
+In this exercise, two example datasets have been pre-loaded into the DataFrames df1 and df2. Only one of them is tidy. Your job is to explore these further in the IPython Shell and identify the one that is not tidy, and why it is not tidy.
+In the rest of this course, you will frequently be asked to explore the structure of DataFrames in the IPython Shell prior to performing different operations on them. Doing this will not only strengthen your comprehension of the data cleaning concepts covered in this course, but will also help you realize and take advantage of the relationship between working in the Shell and in the script.
+
+'''
+
+'''
+Reshaping your data using melt
+Melting data is the process of turning columns of your data into rows of data. Consider the DataFrames from the previous exercise. In the tidy DataFrame, the variables Ozone, Solar.R, Wind, and Temp each had their own column. If, however, you wanted these variables to be in rows instead, you could melt the DataFrame. In doing so, however, you would make the data untidy! This is important to keep in mind: Depending on how your data is represented, you will have to reshape it differently.
+In this exercise, you will practice melting a DataFrame using pd.melt(). There are two parameters you should be aware of: id_vars and value_vars. The id_vars represent the columns of the data you do not want to melt (i.e., keep it in its current shape), while the value_vars represent the columns you do wish to melt into rows. By default, if no value_vars are provided, all columns not set in the id_vars will be melted. This could save a bit of typing, depending on the number of columns that need to be melted.
+The (tidy) DataFrame airquality has been pre-loaded. Your job is to melt its Ozone, Solar.R, Wind, and Temp columns into rows. Later in this chapter, you'll learn how to bring this melted DataFrame back into a tidy form.
+'''
+
+'''
+Print the head of airquality.
+Use pd.melt() to melt the Ozone, Solar.R, Wind, and Temp columns of airquality into rows. Do this by using id_vars to specify the columns you do not wish to melt: 'Month' and 'Day'.
+Print the head of airquality_melt.
+'''
+
 # Print the head of airquality
 print(airquality.head())
 
@@ -7,7 +30,18 @@ airquality_melt = pd.melt(airquality, id_vars=['Month', 'Day'], var_name='measur
 # Print the head of airquality_melt
 print(airquality_melt.head())
 
+'''
+Customizing melted data
+When melting DataFrames, it would be better to have column names more meaningful than variable and value.
+The default names may work in certain situations, but it's best to always have data that is self explanatory.
+You can rename the variable column by specifying an argument to the var_name parameter, and the value column by specifying an argument to the value_name parameter. You will now practice doing exactly this. The DataFrame airquality has been pre-loaded for you.
+'''
 
+'''
+Print the head of airquality.
+Melt the Ozone, Solar.R, Wind, and Temp columns of airquality into rows, with the default variable column renamed to 'measurement' and the default value column renamed to 'reading'. You can do this by specifying, respectively, the var_name and value_name parameters.
+Print the head of airquality_melt.
+'''
 
 # Print the head of airquality_melt
 print(airquality_melt.head())
